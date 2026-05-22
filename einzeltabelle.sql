dd04d3afb9e208d80ecd2f35174c02e9e2f29fdc
gisberger <giglberger.a@gmail.com>
2026-05-22 13:44:25 +0200

    flood2

---
+++ einzeltabelle.sql
@@ -0,0 +1,15 @@
+SELECT a.name_3 AS gemeinde,
+       ROUND(SUM(z."Einwohner"), 0) AS betroffene
+FROM zensus2022_100m z
+JOIN 
+(
+SELECT DISTINCT h.geom
+FROM hwgfhq100_15_04_2026 h 
+JOIN nutzung_aoe n ON st_intersects(n.geom, h.geom)
+WHERE (n.bez LIKE '%Gebäude- und Freifläche Mischnutzung mit Wohnen§' OR
+n.nutzart LIKE '%Wohnbaufläche%')
+) h 
+ON ST_Intersects(h.geom, z.geom)
+JOIN adm_adm_3 a ON ST_Intersects(z.geom, a.geom)
+WHERE a.name_2 LIKE '%Altötting%'
+GROUP BY a.name_3;
